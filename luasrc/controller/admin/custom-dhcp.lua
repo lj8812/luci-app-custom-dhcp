1d973f62be7a1ad0d8ff7d7779fd2f06168fb79f
lj8812 <74142076+lj8812@users.noreply.github.com>
2025-03-19 12:01:53 +0800

    Create custom-dhcp.lua

---
+++ luasrc/controller/admin/custom-dhcp.lua
@@ -0,0 +1,6 @@
+module("luci.controller.admin.custom-dhcp", package.seeall)
+
+function index()
+    -- 直接将菜单名称改为中文（注意文件需保存为 UTF-8 编码）
+    entry({"admin", "services", "custom-dhcp"}, cbi("admin_custom-dhcp/clients"), "专属DHCP服务", 60)
+end
